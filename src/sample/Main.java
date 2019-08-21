3094cb62de98907dc20a12a19ce517faaa9fdf63
vladrph <vrpetithomme2599@eagle.fgcu.edu>
2019-08-21 19:17:17 -0400

    Added !

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,55 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -15,7 +64,10 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        Button
+
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>()
     }
 
 
@@ -23,3 +75,4 @@ public class Main extends Application {
         launch(args);
     }
 }
+*/
